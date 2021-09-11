278da02ae56024e09547da542e235cc0bd097706
dramirez1 <dramirez1@godaddy.com>
2021-09-11 04:50:29 -0500

    latest change

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import {
   groupByByProperty,
-  executeGraphQuery
-} from './utils';
+  executeGraphQuery,
+  getAxieFormatted,
+  filterByAttributes
+} from './utils.js';
+import { CLASSES } from './constants.js';
 
 const getLatestAxiesSold = async ({
   from = 0,
@@ -52,7 +55,7 @@ const getBriefAxieList = async ({
   }
 }) => {
   try {
-    const query = `query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\n  axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {\n    total\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  id\n  name\n  stage\n  class\n  breedCount\n  image\n  title\n  battleInfo {\n    banned\n    __typename\n  }\n  auction {\n    currentPrice\n    currentPriceUSD\n    __typename\n  }\n  parts {\n    id\n    name\n    class\n    type\n    specialGenes\n    __typename\n  }\n  __typename\n}\n`;
+    const query = `query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\n  axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {\n    total\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  id\n  name\n  stage\n  class\n  breedCount\n  genes\n pureness\n title\n  battleInfo {\n    banned\n    __typename\n  }\n  auction {\n    currentPrice\n    currentPriceUSD\n    __typename\n  }\n  parts {\n    id\n    name\n    class\n    type\n    specialGenes\n    __typename\n  }\n  __typename\n}\n`;
     const operationName = `GetAxieBriefList`;
     const variables = {
       auctionType,
@@ -63,9 +66,30 @@ const getBriefAxieList = async ({
     };
 
     const response = await executeGraphQuery({ query, operationName, variables });
+    const { total, results } = response.data.axies;
 
+    return {
+      total,
+      results
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      total: 0,
+      results: []
+    };
+  }
+};
+
+const getAxieInfoMarket = async ({
+  axieId
+}) => {
+  try {
+    const query = `query GetAxieDetail($axieId: ID!) {\n  axie(axieId: $axieId) {\n    ...AxieDetail\n    __typename\n  }\n}\n\nfragment AxieDetail on Axie {\n  id\n  image\n  class\n  chain\n  name\n  genes\n  owner\n  birthDate\n  bodyShape\n  class\n  sireId\n  sireClass\n  matronId\n  matronClass\n  stage\n  title\n  breedCount\n  level\n  figure {\n    atlas\n    model\n    image\n    __typename\n  }\n  parts {\n    ...AxiePart\n    __typename\n  }\n  stats {\n    ...AxieStats\n    __typename\n  }\n  auction {\n    ...AxieAuction\n    __typename\n  }\n  ownerProfile {\n    name\n    __typename\n  }\n  battleInfo {\n    ...AxieBattleInfo\n    __typename\n  }\n  children {\n    id\n    name\n    class\n    image\n    title\n    stage\n    __typename\n  }\n  __typename\n}\n\nfragment AxieBattleInfo on AxieBattleInfo {\n  banned\n  banUntil\n  level\n  __typename\n}\n\nfragment AxiePart on AxiePart {\n  id\n  name\n  class\n  type\n  specialGenes\n  stage\n  abilities {\n    ...AxieCardAbility\n    __typename\n  }\n  __typename\n}\n\nfragment AxieCardAbility on AxieCardAbility {\n  id\n  name\n  attack\n  defense\n  energy\n  description\n  backgroundUrl\n  effectIconUrl\n  __typename\n}\n\nfragment AxieStats on AxieStats {\n  hp\n  speed\n  skill\n  morale\n  __typename\n}\n\nfragment AxieAuction on Auction {\n  startingPrice\n  endingPrice\n  startingTimestamp\n  endingTimestamp\n  duration\n  timeLeft\n  currentPrice\n  currentPriceUSD\n  suggestedPrice\n  seller\n  listingIndex\n  state\n  __typename\n}\n`;
+    const operationName = `GetAxieDetail`;
+    const variables = { axieId };
+    const response = await executeGraphQuery({ operationName, query, variables });
     console.log(response);
-
   } catch (e) {
     console.error(e);
   }
@@ -76,6 +100,43 @@ const getBriefAxieList = async ({
   size: 20
 }); */
 
-getBriefAxieList({
-  size: 100
-});
+/*getBriefAxieList({
+  size: 100,
+  criteria: {
+    classes: [CLASSES.reptile]
+  }
+}); */
+
+const getAllAxiesOnSale = async ({
+  size = 100
+}) => {
+  try {
+    let max = 30000, axies = [];
+    const axiesByClasses = {};
+
+    for (let axieClass of ['Reptile']) {
+      console.log(`Getting ${axieClass}`);
+      for (let from = 0; from < max; from += size) {
+        try {
+          const { total, results } = await getBriefAxieList({
+            from,
+            size,
+            criteria: { classes: [axieClass] }
+          });
+          max = total;
+          axies = [...axies, ...results];
+        } catch (e) {
+          console.error(e);
+        }
+      }
+      axiesByClasses[axieClass] = axies;
+    }
+    const filteredAxies = filterByAttributes({axies: axiesByClasses.Reptile.map(item => getAxieFormatted(item)), filters: { quality: 100 } });
+    console.log(filteredAxies.length);
+    console.log(filteredAxies[0]);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+getAllAxiesOnSale({});
--- utils.js
+++ utils.js
@@ -1,5 +1,16 @@
 import fetch from 'isomorphic-fetch';
-import { BASE_API_URL } from './constants';
+import {
+  BASE_API_URL,
+  CLASS_GENE_MAP,
+  REGION_GENE_MAP,
+  BODY_PARTS,
+  PROBABILITIES,
+  MAX_QUALITY,
+  BINARY_TRAITS,
+  getBodyPartsMap
+} from './constants.js';
+
+var bodyPartsMap = getBodyPartsMap();
 
 const groupByByProperty = ({
   property,
@@ -37,7 +48,191 @@ const executeGraphQuery = async ({
   }
 };
 
+const strMul = ({
+  str,
+  num
+}) => {
+  let s = "";
+  for (let i = 0; i < num; i++) {
+      s += str;
+  }
+  return s;
+};
+
+const genesToBin = genes => {
+  const genesString = genes.toString(2);
+  return strMul({ str: '0', num: 256 - genesString.length }) + genesString;
+};
+
+const getClassFromGroup = group => {
+  const bin = group.slice(0, 4);
+  return !(bin in CLASS_GENE_MAP) ?
+    'Unknown Class' : CLASS_GENE_MAP[bin];
+};
+
+const getRegionFromGroup = group => {
+  const regionBin = group.slice(8,13);
+  return regionBin in REGION_GENE_MAP ?
+    REGION_GENE_MAP[regionBin] : 'Unknown Region';
+};
+
+const getPatternsFromGroup = group => ({
+  d: group.slice(2, 8),
+  r1: group.slice(8, 14),
+  r2: group.slice(14, 20)
+});
+
+// var partsClassMap = {};
+
+const getPartName = ({
+  cls,
+  part,
+  region,
+  binary,
+  skinBinary = '00'
+}) => {
+  let trait;
+  if (binary in BINARY_TRAITS[cls][part]) {
+    if (skinBinary == "11") {
+        trait = BINARY_TRAITS[cls][part][binary]["mystic"];
+    } else if (skinBinary == "10") {
+        trait = BINARY_TRAITS[cls][part][binary]["xmas"];
+    } else if (region in BINARY_TRAITS[cls][part][binary]) {
+        trait = BINARY_TRAITS[cls][part][binary][region];
+    } else if ("global" in BINARY_TRAITS[cls][part][binary]) {
+        trait = BINARY_TRAITS[cls][part][binary]["global"];
+    } else {
+        trait = "UNKNOWN Regional " + cls + " " + part;
+    }
+  } else {
+    trait = "UNKNOWN " + cls + " " + part;
+  }
+  // partsClassMap[trait + " " + part] = cls;
+  return trait;
+};
+
+const getPartFromName = ({
+  traitType,
+  partName
+}) => {
+  let traitId = traitType.toLowerCase() + "-" + partName.toLowerCase().replace(/\s/g, "-").replace(/[\?'\.]/g, "");
+  return bodyPartsMap[traitId];
+};
+
+const getPartsFromGroup = ({
+  part,
+  group,
+  region
+}) => {
+  const skinBinary = group.slice(0, 2);
+  const mystic = skinBinary == "11";
+  const dClass = CLASS_GENE_MAP[group.slice(2, 6)];
+  const dBin = group.slice(6, 12);
+  const dName = getPartName({ cls: dClass, part, region, binary: dBin, skinBinary });
+
+  const r1Class = CLASS_GENE_MAP[group.slice(12, 16)];
+  const r1Bin = group.slice(16, 22);
+  const r1Name = getPartName({ cls: r1Class, part, region, binary: r1Bin });
+
+  const r2Class = CLASS_GENE_MAP[group.slice(22, 26)];
+  const r2Bin = group.slice(26, 32);
+  const r2Name = getPartName({ cls: r2Class, part, region, binary: r2Bin });
+
+  return {
+    d: getPartFromName({ traitType: part, partName: dName }),
+    r1: getPartFromName({ traitType: part, partName: r1Name }),
+    r2: getPartFromName({ traitType: part, partName: r2Name }),
+    mystic: mystic
+  };
+};
+
+const getTraits = genes => {
+  const groups = [
+    genes.slice(0, 32),
+    genes.slice(32, 64),
+    genes.slice(64, 96),
+    genes.slice(96, 128),
+    genes.slice(128, 160),
+    genes.slice(160, 192),
+    genes.slice(192, 224),
+    genes.slice(224, 256)
+  ];
+  const cls = getClassFromGroup(groups[0]);
+  const region = getRegionFromGroup(groups[0]);
+  const pattern = getPatternsFromGroup(groups[1]);
+  let traits = {
+    cls,
+    region,
+    pattern
+  };
+
+  for (let i = 0; i < BODY_PARTS.length; i++) {
+    const bodyPart = BODY_PARTS[i];
+    traits[bodyPart] = getPartsFromGroup({ part: bodyPart, group: groups[i+2], region });
+  }
+
+  return traits;
+};
+
+const getQualityAndPureness = ({
+  traits,
+  cls
+}) => {
+  let quality = 0;
+  for (let i in BODY_PARTS) {
+      if (traits[BODY_PARTS[i]].d.class == cls) {
+          quality += PROBABILITIES.d;
+      }
+      if (traits[BODY_PARTS[i]].r1.class == cls) {
+          quality += PROBABILITIES.r1;
+      }
+      if (traits[BODY_PARTS[i]].r2.class == cls) {
+          quality += PROBABILITIES.r2;
+      }
+  }
+  return { quality: Math.round((quality / MAX_QUALITY) * 100) };
+};
+
+const getAxieFormatted = axie => {
+  if (axie.stage < 3) {
+    return axie;
+  }
+  const genes = genesToBin(BigInt(axie.genes))
+  const traits = getTraits(genes);
+  return {
+    ...axie,
+    traits,
+    ...getQualityAndPureness({ traits, cls: axie.class.toLowerCase() }),
+    genes
+  };
+};
+
+const filterByAttributes = ({
+  axies,
+  filters
+}) => {
+  const filteredAxies = [];
+  for (let axie of axies) {
+    let match = false;
+    for (let [key, value] of Object.entries(filters)) {
+      if (key === 'breedCount') {
+        match = axie[key] === value;
+      } else if (key === 'price') {
+        match = parseFloat(axie.auction.currentPriceUSD, 10) < value;
+      } else if (key === 'quality') {
+        match = axie[key] === value;
+      }
+    }
+    if (match === true) {
+      filteredAxies.push(axie);
+    }
+  }
+  return filteredAxies;
+};
+
 export {
   groupByByProperty,
-  executeGraphQuery
+  executeGraphQuery,
+  getAxieFormatted,
+  filterByAttributes
 };
